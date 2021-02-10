987546600baabfb057b8546df1e4897eed797a11
DanielNeale <danielneale01@gmail.com>
2021-02-10 18:24:01 +0000

    Created basic camera system

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float sensitivity;
+    [SerializeField]
+    private int maxY;
+    [SerializeField]
+    private int minY;
+
+    private Transform cam;
+    private float xMove;
+    private float yMove;
+
+
+    private void Start()
+    {
+        cam = transform.GetChild(0);
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(transform.forward.x, 0, transform.forward.z) * speed;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(transform.forward.x, 0, transform.forward.z) * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(transform.right.x, 0, transform.right.z) * speed;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(transform.right.x, 0, transform.right.z) * speed;
+        }
+
+
+        if (!(yMove - Input.GetAxis("Mouse Y") * sensitivity > maxY) && !(yMove - Input.GetAxis("Mouse Y") * sensitivity < minY))
+        {
+            yMove -= Input.GetAxis("Mouse Y") * sensitivity;
+        }
+
+        xMove += Input.GetAxis("Mouse X") * sensitivity;
+
+        transform.rotation = Quaternion.Euler(0, xMove, 0);
+        cam.rotation = Quaternion.Euler(yMove, xMove, 0);
+    }
+}
