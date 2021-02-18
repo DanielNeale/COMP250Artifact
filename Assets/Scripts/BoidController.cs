a4363ec18dc38400875f1b2b631e4ab03b5d849e
DanielNeale <danielneale01@gmail.com>
2021-02-18 01:13:06 +0000

    Started work on boid movement

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+    private Vector3 moveDir;
+    [SerializeField]
+    private float speed = 1;
+    private Rigidbody rb;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        moveDir = Vector3.forward;
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(moveDir * speed, ForceMode.VelocityChange);
+    }
+}
